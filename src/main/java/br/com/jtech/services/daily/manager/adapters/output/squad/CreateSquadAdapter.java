89ea3b5c5dc55022dd0ee3f64946eab75c51b532
filipecsoares <filipecarlossoares@gmail.com>
2023-12-07 09:12:01 -0300

    feat: create squad

--- src/main/java/br/com/jtech/services/daily/manager/adapters/output/squad/CreateSquadAdapter.java
+++ src/main/java/br/com/jtech/services/daily/manager/adapters/output/squad/CreateSquadAdapter.java
@@ -1,5 +1,7 @@
 package br.com.jtech.services.daily.manager.adapters.output.squad;
 
+import br.com.jtech.services.daily.manager.adapters.output.repositories.SquadRepository;
+import br.com.jtech.services.daily.manager.application.core.domains.squad.Squad;
 import br.com.jtech.services.daily.manager.application.ports.output.squad.CreateSquadOutputGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CreateSquadAdapter implements CreateSquadOutputGateway {
+
+    private final SquadRepository repository;
+
+    @Override
+    public Squad create(Squad request) {
+        var document = this.repository.save(request.toDocument());
+        return Squad.fromDocument(document);
+    }
 }
--- src/main/java/br/com/jtech/services/daily/manager/application/core/domains/squad/Squad.java
+++ src/main/java/br/com/jtech/services/daily/manager/application/core/domains/squad/Squad.java
@@ -3,8 +3,11 @@ package br.com.jtech.services.daily.manager.application.core.domains.squad;
 import br.com.jtech.services.daily.manager.adapters.input.protocols.squad.SquadRequest;
 import br.com.jtech.services.daily.manager.adapters.output.repositories.entities.squad.SquadDocument;
 import br.com.jtech.services.daily.manager.application.core.domains.employee.Employee;
+import br.com.jtech.services.daily.manager.config.infra.utils.GenId;
 import lombok.*;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Member;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,11 +25,30 @@ public class Squad {
     private Integer maxCapacity;
     private Boolean isPublic;
 
-    public static Squad fromDocument(SquadDocument squad) {
-        return null;
+    public List<Employee> getMembers() {
+        return members != null ? members : List.of();
+    }
+    public static Squad fromDocument(SquadDocument document) {
+        var squad = new Squad();
+        BeanUtils.copyProperties(document, squad);
+        return squad;
     }
 
-    public static Squad fromRequest(SquadRequest squad) {
-        return null;
+    public static Squad fromRequest(SquadRequest request) {
+        var squad = new Squad();
+        BeanUtils.copyProperties(request, squad);
+        squad.setId(GenId.newUuid(request.getId()));
+        return squad;
+    }
+
+    public SquadDocument toDocument() {
+        return SquadDocument.builder().
+                id(getId()).
+                name(getName()).
+                description(getDescription()).
+                members(getMembers()).
+                maxCapacity(getMaxCapacity()).
+                isPublic(getIsPublic()).
+                build();
     }
 }
