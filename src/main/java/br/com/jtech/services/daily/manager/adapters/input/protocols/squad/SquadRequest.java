4bc1b7d94de1a68ec2a8399081af3b91fecb4681
filipecsoares <filipecarlossoares@gmail.com>
2023-12-09 15:10:23 -0300

    [feature-squad] test: create tests for squad creation

--- src/main/java/br/com/jtech/services/daily/manager/adapters/input/protocols/squad/SquadRequest.java
+++ src/main/java/br/com/jtech/services/daily/manager/adapters/input/protocols/squad/SquadRequest.java
@@ -2,6 +2,7 @@ package br.com.jtech.services.daily.manager.adapters.input.protocols.squad;
 
 import br.com.jtech.services.daily.manager.adapters.input.protocols.employee.EmployeeRequest;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SquadRequest implements Serializable {
     private String id;
+    @NotBlank(message = "Name cannot be empty!")
     private String name;
     private String description;
     private List<EmployeeRequest> members;
---
+++ src/test/java/br/com/jtech/services/daily/manager/adapters/output/squad/CreateSquadAdapterTest.java
@@ -0,0 +1,63 @@
+package br.com.jtech.services.daily.manager.adapters.output.squad;
+
+import br.com.jtech.services.daily.manager.adapters.output.repositories.SquadRepository;
+import br.com.jtech.services.daily.manager.adapters.output.repositories.entities.squad.SquadDocument;
+import br.com.jtech.services.daily.manager.application.core.domains.employee.Employee;
+import br.com.jtech.services.daily.manager.application.core.domains.squad.Squad;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class CreateSquadAdapterTest {
+
+    @Mock
+    private SquadRepository repository;
+
+    @InjectMocks
+    private CreateSquadAdapter createSquadAdapter;
+
+    @Test
+    public void testCreateSquad() {
+        var id = UUID.randomUUID();
+        String name = "Squad 1";
+        String description = "Squad 1 description";
+        int maxCapacity = 10;
+        boolean isPublic = true;
+        List<Employee> members = List.of();
+        var mockSquad = Squad.builder()
+                .id(id)
+                .name(name)
+                .description(description)
+                .members(members)
+                .maxCapacity(maxCapacity)
+                .isPublic(isPublic)
+                .build();
+        var mockSquadDocument = SquadDocument.builder()
+                .id(id)
+                .name(name)
+                .description(description)
+                .members(members)
+                .maxCapacity(maxCapacity)
+                .isPublic(isPublic)
+                .build();
+        when(repository.save(any(SquadDocument.class))).thenReturn(mockSquadDocument);
+
+        var createdSquad = createSquadAdapter.create(mockSquad);
+
+        verify(repository, times(1)).save(any(SquadDocument.class));
+        assertEquals(id, createdSquad.getId());
+        assertEquals(name, createdSquad.getName());
+        assertEquals(description, createdSquad.getDescription());
+        assertEquals(maxCapacity, createdSquad.getMaxCapacity());
+        assertEquals(isPublic, createdSquad.getIsPublic());
+    }
+}
